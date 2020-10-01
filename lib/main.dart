0a316dd6935999ae4c6fd3b70a6ce0fd18f9b985
Keyur Kanjiya <kanjiyakeyur01@gmail.com>
2020-10-01 21:16:12 +0530

    auto change inside circle and triangle or redius is controll by scroller

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        canvasColor: Colors.black,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: TextAnimation(),
--- lib/test.dart
+++ lib/test.dart
@@ -10,16 +10,12 @@ class _TextAnimationState extends State<TextAnimation>
     with TickerProviderStateMixin {
   Animation<double> animation;
   AnimationController controller;
-  Animation<double> rediusAnimation;
   Tween<double> _rotationTween = Tween(begin: -math.pi, end: math.pi);
-  Tween<double> _rediusTween = Tween(begin: 0, end: 200);
-  int _side = 3;
-  double _redius = 50;
+  double _redius = 100;
 
   @override
   void initState() {
     super.initState();
-
     controller =
         AnimationController(vsync: this, duration: Duration(seconds: 5));
 
@@ -29,17 +25,7 @@ class _TextAnimationState extends State<TextAnimation>
       })
       ..addStatusListener((status) {
         if (status == AnimationStatus.completed) {
-          controller.reverse();
-        } else if (status == AnimationStatus.dismissed) {
-          controller.forward();
-        }
-      });
-
-    rediusAnimation = _rediusTween.animate(controller)
-      ..addListener(() {})
-      ..addStatusListener((status) {
-        if (status == AnimationStatus.completed) {
-          controller.reverse();
+          controller.repeat();
         } else if (status == AnimationStatus.dismissed) {
           controller.forward();
         }
@@ -49,48 +35,39 @@ class _TextAnimationState extends State<TextAnimation>
   }
 
   @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
           Expanded(
             child: CustomPaint(
-              painter:
-                  ShapePainter(rediusAnimation.value, _side, animation.value),
+              painter: ShapePainter(_redius, 50, animation.value),
               child: Container(),
             ),
           ),
           Text(
-            'sides ' + _side.toString(),
-            style: TextStyle(color: Colors.white),
-          ),
-          Slider(
-            value: _side.toDouble(),
-            max: 10,
-            label: _side.toString(),
-            divisions: 10,
-            onChanged: (value) {
-              setState(() {
-                _side = value.toInt();
-              });
-            },
-          ),
-          Text(
-            'redius ' + rediusAnimation.value.toString(),
+            'redius ',
             style: TextStyle(color: Colors.white),
           ),
           Slider(
             value: _redius,
-            max: 250,
+            max: 200,
             onChanged: (value) {
               setState(() {
                 _redius = value;
               });
             },
-          ),
-          Text(
-            'Rotation' + animation.value.toString(),
-            style: TextStyle(color: Colors.white),
           ),
         ],
       ),
@@ -99,38 +76,69 @@ class _TextAnimationState extends State<TextAnimation>
 }
 
 class ShapePainter extends CustomPainter {
-  final double redius;
-  final int side;
+  final double radius;
+  final double radius2;
   final double rotation;
-  ShapePainter(this.redius, this.side, this.rotation);
+  ShapePainter(this.radius, this.radius2, this.rotation);
   @override
   void paint(Canvas canvas, Size size) {
-    var paint = Paint()
+    var paint1 = Paint()
       ..color = Colors.teal
       ..strokeWidth = 2
       ..style = PaintingStyle.stroke
       ..strokeCap = StrokeCap.round;
 
-    var path = Path();
-    var angle = (math.pi * 2) / side;
+    var paint2 = Paint()
+      ..color = Colors.red
+      ..strokeWidth = 20
+      ..style = PaintingStyle.fill
+      ..strokeCap = StrokeCap.round;
+
+    var paint3 = Paint()
+      ..color = Colors.yellow
+      ..strokeWidth = 2
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round;
+
+    var paint4 = Paint()
+      ..color = Colors.cyan
+      ..strokeWidth = 2
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round;
+
     Offset center = Offset(size.width / 2, size.height / 2);
+    Offset dotPosittion = Offset(radius * math.cos(rotation) + center.dx,
+        radius * math.sin(rotation) + center.dy);
+    canvas.drawCircle(center, radius, paint1);
+    canvas.drawCircle(dotPosittion, 5, paint2);
+    canvas.drawCircle(
+        center,
+        math.cos(rotation) >= 0
+            ? radius * math.cos(rotation)
+            : -radius * math.cos(rotation),
+        paint3);
+    canvas.drawLine(center,
+        Offset(radius * math.cos(rotation) + center.dx, center.dy), paint4);
+    canvas.drawLine(center, dotPosittion, paint4);
+    canvas.drawLine(Offset(radius * math.cos(rotation) + center.dx, center.dy),
+        dotPosittion, paint4);
 
-    Offset startPoint =
-        Offset(redius * math.cos(rotation), redius * math.sin(rotation));
-
-    path.moveTo(startPoint.dx + center.dx, startPoint.dy + center.dy);
-
-    for (int i = 1; i <= side; i++) {
-      double dx = redius * math.cos(angle * i + rotation) + center.dx;
-      double dy = redius * math.sin(angle * i + rotation) + center.dy;
-      path.lineTo(dx, dy);
-    }
-    canvas.drawPath(path, paint);
+    final textspan = TextSpan(
+        text:
+            "(${(dotPosittion.dx - center.dx).round()},${(dotPosittion.dy - center.dy).round()})",
+        style: TextStyle(color: Colors.black, fontSize: 16));
+    final txtPrinter = TextPainter(
+      text: textspan,
+      textDirection: TextDirection.ltr,
+    );
+    txtPrinter.layout(minWidth: 0, maxWidth: 100);
+    txtPrinter.paint(canvas, dotPosittion);
+    //canvas.drawLine(center, Offset(center.dx - radius, center.dy), paint4);
   }
 
   @override
   bool shouldRepaint(CustomPainter oldDeleget) {
-    return false;
+    return true;
   }
 }
 
